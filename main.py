20a812dfdf97ebfa9522394eba673b0207f79ab4
Toni Sredanović <toni@krakensystems.co>
2020-12-06 20:01:43 +0100

    handle test and playback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 def create_app():
@@ -8,7 +8,35 @@ def create_app():
 
     @app.route('/emby_webhook', methods=['POST'])
     def index():
-        print('GOT SOMETHING')
+        request_json = request.get_json()
+        event = request_json.get('Event')
+        if not event:
+            print('Missing event.')
+        print('Got event: {}'.format(event))
+        event_category = event.split('.')[0]
+        event_action = event.split('.')[1]
+
+        message = None
+
+        # System
+        if event_category == 'system':
+            if event_action == 'webhooktest':
+                message = 'Test.'
+
+        # Playback
+        elif event_category == 'playback':
+            user_name = request_json.get('User', {}).get('Name', None)
+            item_name = request_json.get('Item', {}).get('Name', None)
+            if event_action == 'start':
+                message = 'User `{}` started playing `{}`.'.format(user_name, item_name)
+            elif event_action == 'pause':
+                message = 'User `{}` paused `{}`.'.format(user_name, item_name)
+            elif event_action == 'unpause':
+                message = 'User `{}` unpaused `{}`.'.format(user_name, item_name)
+            elif event_action == 'stop':
+                message = 'User `{}` stopped `{}`.'.format(user_name, item_name)
+
+        print(message)
         return ''
 
     return app
